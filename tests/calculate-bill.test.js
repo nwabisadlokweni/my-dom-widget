477e5d6783f9d9462171f8cd47fea661cb017bca
nwabisadlokweni <dlonwa001@gmail.com>
2020-06-30 13:29:18 +0200

    change

--- tests/calculate-bill.test.js
+++ tests/calculate-bill.test.js
@@ -1,5 +1,5 @@
 describe("The calculate bill factory function", function () {
-    it("should be able to set call cost", function () {
+    it("should be able to set call cost and sms cost", function () {
         let calculate = totalPhoneBill("call, sms, call, sms");
 
         assert.equal(7.00, calculate);
